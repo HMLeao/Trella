21d88a09a9392385e36a987eaa9102e34c04563b
Henrique Martins <leaomartinshenrique@gmail.com>
2019-10-17 02:29:23 -0300

    mostrando mensagens de erro

--- js/login.js
+++ js/login.js
@@ -6,17 +6,22 @@ change.innerHTML = 'Cadastre-se';
 changeMode.innerHTML = 'Ainda não tem conta? ';
 change.addEventListener('click',function(event) {
     event.preventDefault();
+    var alertElement;
     if(loginForm.style.display == 'block') {
         loginForm.style.display = 'none';
         signUpForm.style.display = 'block';
         this.innerHTML = 'Entrar';
         changeMode.innerHTML = 'Já é cadastrado? ';
+        alertElement = document.getElementById('sign-in-error');
+        if(alertElement) {
+            alertElement.parentNode.removeChild(alertElement);
+        }
     } else {
         loginForm.style.display = 'block';
         signUpForm.style.display = 'none';
         changeMode.innerHTML = 'Ainda não tem conta? ';
         this.innerHTML = 'Cadastre-se';
-        var alertElement = document.getElementById('sign-up-success');
+        alertElement = document.getElementById('sign-up-success');
         if(alertElement) {
             alertElement.parentNode.removeChild(alertElement);
         }
@@ -30,23 +35,64 @@ signUpForm.addEventListener('submit',function(e) {
         username:'',
         password:''
     };
+
+    var alertElement = document.getElementById('sign-in-error');
+    if(alertElement) {
+        alertElement.parentNode.removeChild(alertElement);
+    }
+    
     user.name = document.getElementById('name').value;
     user.username = document.getElementById('username').value;
     user.password = document.getElementById('password').value;
-    postJsonData('https://tads-trello.herokuapp.com/api/trello/users/new',JSON.stringify(user),function(response) {
-        var mainDiv = document.getElementById('main-content');
-        var successAlert = document.createElement('div');
-        successAlert.classList.add('alert','alert-success','success-alert');
-        successAlert.setAttribute('role','alert');
-        successAlert.setAttribute('id','sign-up-success');
-        successAlert.innerHTML = 'Cadastro realizado com sucesso! Faça login na sua conta!';
-        mainDiv.appendChild(successAlert);
-        loginForm.style.display = 'block';
-        signUpForm.style.display = 'none';
-        changeMode.innerHTML = 'Ainda não tem conta? ';   // <- Tá esquisito isso. Código repetido!!!!
-        change.innerHTML = 'Cadastre-se';
-    });
 
+    if (!user.name || !user.username || !user.password) {
+        var mainDiv = document.getElementById('main-content');
+        var errorAlert = document.createElement('div');
+        errorAlert.classList.add('alert','alert-danger','success-alert');
+        errorAlert.setAttribute('role','alert');
+        errorAlert.setAttribute('id','sign-in-error');
+        errorAlert.innerHTML = 'Os campos são obrigatórios!';
+        mainDiv.appendChild(errorAlert);
+    } else {
+        postJsonData('https://tads-trello.herokuapp.com/api/trello/users/new',JSON.stringify(user),function(response) {
+            var mainDiv = document.getElementById('main-content');
+            var successAlert = document.createElement('div');
+            successAlert.classList.add('alert','alert-success','success-alert');
+            successAlert.setAttribute('role','alert');
+            successAlert.setAttribute('id','sign-up-success');
+            successAlert.innerHTML = 'Cadastro realizado com sucesso! Faça login na sua conta!';
+            mainDiv.appendChild(successAlert);
+            loginForm.style.display = 'block';
+            signUpForm.style.display = 'none';
+            changeMode.innerHTML = 'Ainda não tem conta? ';   // <- Tá esquisito isso. Código repetido!!!!
+            change.innerHTML = 'Cadastre-se';
+        });
+    }
+    
+
+});
+
+loginForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var user = {
+        name:'',
+        password:''
+    };
+    var alertElement = document.getElementById('sign-in-error');
+    if(alertElement) {
+        alertElement.parentNode.removeChild(alertElement);
+    }
+    user.name = document.getElementById('username-login').value;
+    user.password = document.getElementById('password-login').value;
+    if(!user.name || !user.password) {
+        var mainDiv = document.getElementById('main-content');
+        var errorAlert = document.createElement('div');
+        errorAlert.classList.add('alert','alert-danger','success-alert');
+        errorAlert.setAttribute('role','alert');
+        errorAlert.setAttribute('id','sign-in-error');
+        errorAlert.innerHTML = 'Os campos são obrigatórios!';
+        mainDiv.appendChild(errorAlert);
+    }
 });
 
 function postJsonData(url,jsonData,successCallback){
@@ -60,3 +106,4 @@ function postJsonData(url,jsonData,successCallback){
     xhttp.setRequestHeader('Content-Type', 'application/json');
     xhttp.send(jsonData);
 }
+
